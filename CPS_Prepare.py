5d354bdd5e292d46cb34028d881a8b052601b51e
MyriamArnal <myriama@gmail.com>
2023-02-05 11:24:02 +0100

    modified:   CPS_Prepare.py add plots in excel export

--- CPS_Prepare.py
+++ CPS_Prepare.py
@@ -14,18 +14,25 @@ from itertools import groupby
 import linecache
 
 @st.cache
+
+def excel_write_col(worksheet,row,col, data):
+    print(len(data))
+
+    for row_count in range (row,len(data)+row):
+        worksheet.write(row_count,col,data[row_count-row])
+
 def save_raw_excel(result_5, result_079, data):
     output = BytesIO()
     workbook = xlsxwriter.Workbook(output, {'in_memory': True})
     bold = workbook.add_format({'bold': 1})
-    
-    # Worksheet result 
+
+    # Worksheet result
     worksheet_1 = workbook.add_worksheet('Results')
-    row = 1
-    col = 0
-    row_i =3
-    col_i =3
-    
+    row = 1 #row counter for tab 1
+    col = 0 #col counter for tab 1
+    row_i =3 # initial row for tabs
+    col_i =3 # initial col for tabs
+
     parameters = result_5.head()
     worksheet_1.write(row_i-1, col_i, "Result 5 microns",bold)
     worksheet_1.write_row(row_i, col_i, parameters)
@@ -33,29 +40,82 @@ def save_raw_excel(result_5, result_079, data):
         worksheet_1.write(row_i + row ,col_i -1, index)
         worksheet_1.write_row(row_i + row, col_i , result_5.loc[index])
         row += 1
-        
+
     worksheet_1.write(row_i + row, col_i, "Result 0.79 microns", bold)
     row += 1
-    for index in result_079.index :
+    for index in result_5.index :
         worksheet_1.write(row_i + row ,col_i -1, index)
         worksheet_1.write_row(row_i + row, col_i , result_079.loc[index])
         row += 1
- 
-    # Worksheet result 
+
+    row_data = row + 25 # Row to start data table in tab 1
+    chart1 = workbook.add_chart({'type': 'scatter', 'subtype': 'smooth'})
+    chart2 = workbook.add_chart({'type': 'scatter', 'subtype': 'smooth'})
+    chart3 = workbook.add_chart({'type': 'scatter', 'subtype': 'smooth'})
+
+    # Worksheet result
     mydict = {}
+    series = {}
+    col_d = col_i
     for sample in data :
         name = sample['name']
-        mydict[f"worksheet_{name}"] =  workbook.add_worksheet(name)  
-        col =0
         data_sample = sample['data']
-        for df_col in list(data_sample) : 
-            mydict[f"worksheet_{name}"].write(row_i-1,col_i + col,df_col, bold) 
-            mydict[f"worksheet_{name}"].write_column(row_i,col_i + col,data_sample[df_col])
-            col+=1
+
+        #Write subset of data in first  tab
+        len_data = len(data_sample["Diameter"].values.tolist())
+        worksheet_1.write(row_data -2  ,col_d , name ,bold)
+        worksheet_1.write(row_data -1  ,col_d , "Diameter",bold)
+        worksheet_1.write(row_data -1  ,col_d +1 , "Weight_Height_norm",bold)
+        worksheet_1.write(row_data -1  ,col_d + 2 , "Weight_LogW_norm",bold)
+        worksheet_1.write(row_data -1  ,col_d + 3 , "Weight_CumWt_norm",bold)
+        excel_write_col(worksheet_1, row_data,col_d, data_sample["Diameter"].values.tolist())
+        excel_write_col(worksheet_1, row_data,col_d + 1, data_sample["Weight_Height_norm"].values.tolist())
+        excel_write_col(worksheet_1, row_data,col_d + 2, data_sample["Weight_LogW_norm"].values.tolist())
+        excel_write_col(worksheet_1, row_data,col_d + 3, data_sample["Weight_CumWt_norm"].values.tolist())
+
+        # Add series to plots
+        diam_cat = f"={name}!${xlsxwriter.utility.xl_col_to_name(col_i)}${row_i+1}:${xlsxwriter.utility.xl_col_to_name(col_i)}${len_data}"
+        WH_val = f"={name}!${xlsxwriter.utility.xl_col_to_name(col_i+1)}${row_i+1}:${xlsxwriter.utility.xl_col_to_name(col_i+1)}${len_data}"
+        WLW_val = f"={name}!${xlsxwriter.utility.xl_col_to_name(col_i+2)}${row_i+1}:${xlsxwriter.utility.xl_col_to_name(col_i+2)}${len_data}"
+        WCW_val = f"={name}!${xlsxwriter.utility.xl_col_to_name(col_i+3)}${row_i+1}:${xlsxwriter.utility.xl_col_to_name(col_i+3)}${len_data}"
+        chart1.add_series({'name': name,'categories':diam_cat ,'values': WH_val,})
+        chart2.add_series({'name': name,'categories':diam_cat ,'values': WLW_val,})
+        chart3.add_series({'name': name,'categories':diam_cat ,'values': WCW_val,})
+
+        #Write data, one dataset by tab
+        mydict[f"worksheet_{name}"] =  workbook.add_worksheet(name)
+        col_tabs =0 # col counter for additional tabs
+        for df_col in list(data_sample) :
+            mydict[f"worksheet_{name}"].write(row_i-1,col_i + col_tabs,df_col, bold)
+            mydict[f"worksheet_{name}"].write_column(row_i,col_i + col_tabs,data_sample[df_col])
+            col_tabs+=1
+        col_d +=4
+
+    # Add a chart title and some axis labels.
+    chart1.set_title ({'name': 'Weight Height Distribution'})
+    chart1.set_x_axis({'name': 'Diameter  [microns]','max':500})
+    chart1.set_y_axis({'name': 'Normalized counts'})
+
+    chart2.set_title ({'name': 'LogW Distribution'})
+    chart2.set_x_axis({'name': 'Diameter  [microns]','max':500})
+    chart2.set_y_axis({'name': 'Normalized counts'})
+
+    chart3.set_title ({'name': 'Cumulative Weight Distribution'})
+    chart3.set_x_axis({'name': 'Diameter  [microns]','max':500})
+    chart3.set_y_axis({'name': 'Normalized counts'})
+
+    # Set an Excel chart style.
+    #chart1.set_style(1)
+    #chart2.set_style(2)
+    #chart3.set_style(3)
+    # Insert the chart into the worksheet (with an offset).
+    worksheet_1.insert_chart('B13', chart1, {'x_offset': 0, 'y_offset': 0})
+    worksheet_1.insert_chart('J13', chart2, {'x_offset': 0, 'y_offset': 0})
+    worksheet_1.insert_chart('R13', chart3, {'x_offset': 0, 'y_offset': 0})
     workbook.close()
 
     return output
-    
+
 
 
 def group_index(data) :
@@ -63,21 +123,21 @@ def group_index(data) :
     for k,g in groupby(enumerate(data),lambda x:x[0]-x[1]):
         group = (map(itemgetter(1),g))
         group = list(map(int,group))
-        ranges.append((group[0],group[-1]))  
+        ranges.append((group[0],group[-1]))
     return ranges
 
 def CSP_parameters(data):
-    # Sur distribution cumulée : Weight_CumWt_norm    
+    # Sur distribution cumulée : Weight_CumWt_norm
     distr_values = [90.,84.,75.,50.,25.,16.,10.]
     Values = np.interp(distr_values,  data['Weight_CumWt_norm'], data['Diameter'])
     Ld = (Values[5]-Values[1])/Values[3]
-    Values = np.append(Values,Ld) 
-    
+    Values = np.append(Values,Ld)
+
     # Sur distribution : Weight_Height_norm
     mode_index = data['Weight_Height_norm'].idxmax()
-    mode = data['Diameter'][mode_index] 
-    Values = np.append(Values,mode) 
-    
+    mode = data['Diameter'][mode_index]
+    Values = np.append(Values,mode)
+
     #Find where Weight_Height_norm = 50
     norm_50 = np.where(np.abs(data['Weight_Height_norm'] - 50. ) < 10. )
     index_group_50 = group_index(norm_50[:][0])
@@ -91,18 +151,18 @@ def CSP_parameters(data):
         t = np.squeeze(np.abs(index_50 - mode) )
         t_sort = np.argsort(t,axis=0)
         tmp = index_50[t_sort[:][1]] - index_50[t_sort[:][0]]
-        fwhm = tmp 
+        fwhm = tmp
     else:
-        fwhm = np.nan 
-    Values = np.append(Values,fwhm)    
-    Values = np.append(Values,fwhm/mode) 
+        fwhm = np.nan
+    Values = np.append(Values,fwhm)
+    Values = np.append(Values,fwhm/mode)
     return Values
 
 
-# main
+######### Main  #########
 st.set_page_config(layout="wide", page_title="Du coté de chez Swan", page_icon="pics/swan.png")
 st.sidebar.image("pics/swan.png")
-st.sidebar.title('Swan - CPS')   
+st.sidebar.title('Swan - CPS')
 st.sidebar.write("Preparation des **données CPS** et calcul des parametres pour la methode 5microns et 0.79 microns . Le programme accepte en entrée les fichiers natifs produit par **blabla** .")
 
 
@@ -114,45 +174,44 @@ Data_all = list()
 Parameter = ['D10','D16','D25','D50','D75','D84','D90','Ld','Mode','FWHM','FWHM/Mode']
 
 index =[]
-for file in uploaded_files : 
+for file in uploaded_files :
     sample_name = file.name.replace('.csv','')
     index.append(sample_name)
 
 result_5microns = pd.DataFrame(columns = Parameter ,  index = index)
 result_079microns = pd.DataFrame(columns = Parameter ,  index = index)
 
-for file in uploaded_files : 
-    
+for file in uploaded_files :
     sample_name = file.name.replace('.csv','')
-    
+
     data = pd.read_csv(file, sep=",", skiprows=11, names=colnames, header=None)
     data.drop(['Empty1' , 'Empty2', 'Empty3' , 'Empty4', 'Empty5'], axis = 1)
-    
+
     data['Diameter'] = data['Diameter'] * 1000
-    
+
     data_079 = data.loc[data['Diameter'] < 790].copy(deep=False).reset_index()
-    
-    data.insert(1, 'Absorbance_CumNum_norm', 100 * data['Absorbance_CumNum'] / data['Absorbance_CumNum'].max() ) 
-    data.insert(1, 'Absorbance_LogNum_norm', 100 * data['Absorbance_LogNum'] / data['Absorbance_LogNum'].max() ) 
-    data.insert(1, 'Absorbance_Height_norm', 100 * data['Absorbance_Height'] / data['Absorbance_Height'].max() ) 
-    data.insert(1, 'Number_CumSurf_norm', 100 * data['Number_CumSurf']/ data['Number_CumSurf'].max() ) 
-    data.insert(1, 'Number_LogSur_norm', 100 * data['Number_LogSur'] / data['Number_LogSur'].max() ) 
-    data.insert(1, 'Number_Height_norm', 100 * data['Number_Height'] / data['Number_Height'].max() ) 
-    data.insert(1, 'Surface_CumSurf_norm', 100 * data['Surface_CumSurf'] / data['Surface_CumSurf'].max() ) 
-    data.insert(1, 'Surface_LogSur_norm', 100 * data['Surface_LogSur']/ data['Surface_LogSur'].max() ) 
-    data.insert(1, 'Surface_Height_norm', 100 * data['Surface_Height']/ data['Surface_Height'].max() ) 
-    data.insert(1, 'Weight_CumWt_norm', 100 * data['Weight_CumWt']/ data['Weight_CumWt'].max() ) 
-    data.insert(1, 'Weight_LogW_norm', 100 * data['Weight_LogW']/ data['Weight_LogW'].max() ) 
-    data.insert(1, 'Weight_Height_norm', 100 * data['Weight_Height'] / data['Weight_Height'].max() ) 
- 
-    data_079.insert(1, 'Weight_CumWt_norm', data_079['Weight_CumWt'] - data_079['Weight_CumWt'][0] ) 
-    data_079['Weight_CumWt_norm'] *= 100 / data_079['Weight_CumWt_norm'].max() 
-    data_079.insert(1, 'Weight_LogW_norm', 100 * data_079['Weight_LogW']/ data_079['Weight_LogW'].max() ) 
+
+    data.insert(1, 'Absorbance_CumNum_norm', 100 * data['Absorbance_CumNum'] / data['Absorbance_CumNum'].max() )
+    data.insert(1, 'Absorbance_LogNum_norm', 100 * data['Absorbance_LogNum'] / data['Absorbance_LogNum'].max() )
+    data.insert(1, 'Absorbance_Height_norm', 100 * data['Absorbance_Height'] / data['Absorbance_Height'].max() )
+    data.insert(1, 'Number_CumSurf_norm', 100 * data['Number_CumSurf']/ data['Number_CumSurf'].max() )
+    data.insert(1, 'Number_LogSur_norm', 100 * data['Number_LogSur'] / data['Number_LogSur'].max() )
+    data.insert(1, 'Number_Height_norm', 100 * data['Number_Height'] / data['Number_Height'].max() )
+    data.insert(1, 'Surface_CumSurf_norm', 100 * data['Surface_CumSurf'] / data['Surface_CumSurf'].max() )
+    data.insert(1, 'Surface_LogSur_norm', 100 * data['Surface_LogSur']/ data['Surface_LogSur'].max() )
+    data.insert(1, 'Surface_Height_norm', 100 * data['Surface_Height']/ data['Surface_Height'].max() )
+    data.insert(1, 'Weight_CumWt_norm', 100 * data['Weight_CumWt']/ data['Weight_CumWt'].max() )
+    data.insert(1, 'Weight_LogW_norm', 100 * data['Weight_LogW']/ data['Weight_LogW'].max() )
+    data.insert(1, 'Weight_Height_norm', 100 * data['Weight_Height'] / data['Weight_Height'].max() )
+
+    data_079.insert(1, 'Weight_CumWt_norm', data_079['Weight_CumWt'] - data_079['Weight_CumWt'][0] )
+    data_079['Weight_CumWt_norm'] *= 100 / data_079['Weight_CumWt_norm'].max()
+    data_079.insert(1, 'Weight_LogW_norm', 100 * data_079['Weight_LogW']/ data_079['Weight_LogW'].max() )
     data_079.insert(1, 'Weight_Height_norm', 100 * data_079['Weight_Height']/ data_079['Weight_Height'].max() )
- 
+
     result_5microns.loc[sample_name] = CSP_parameters(data)
     result_079microns.loc[sample_name] = CSP_parameters(data_079)
-  
+
     Data_all.append({'name':sample_name, 'data': data})
 
 #Print results
@@ -166,19 +225,19 @@ with st.container() :
     st.write('Methode 0.79 microns')
     st.dataframe(result_079microns.style.format("{:.1f}"))
 
-    
-#Plot figures    
+
+#Plot figures
 fig = make_subplots(rows=3, cols=1,
                     shared_xaxes=True,
                     vertical_spacing=0.02)
 col_scale = px.colors.qualitative.Dark24
-i =0                     
+i =0
 for sample in Data_all :
     fig.add_trace(go.Scatter( x=sample['data']["Diameter"], y=sample['data']["Weight_Height_norm"], name = sample['name'], legendgroup='group1', line=dict(color=col_scale[i], width=2)),row=3, col=1)
     fig.add_trace(go.Scatter( x=sample['data']["Diameter"], y=sample['data']["Weight_LogW_norm"], name = sample['name'], legendgroup='group2',showlegend=False, line=dict(color=col_scale[i],width=2)),row=2, col=1)
     fig.add_trace(go.Scatter( x=sample['data']["Diameter"], y=sample['data']["Weight_CumWt_norm"], name = sample['name'], legendgroup='group3', showlegend=False, line=dict(color=col_scale[i], width=2)),row=1, col=1)
     i+=1
-    
+
 fig.update_yaxes(title_text="Height", row=3, col=1)
 fig.update_yaxes(title_text="LogW", row=2, col=1)
 fig.update_yaxes(title_text="CumWt", row=1, col=1)
@@ -189,8 +248,7 @@ fig.update_layout(
     autosize=False,
     width=800,
     height=800,)
-                           
+
 st.plotly_chart(fig)
 
 st.download_button( label="Download Excel workbook", data=save_raw_excel(result_5microns, result_079microns, Data_all).getvalue(), file_name="workbook.xlsx", mime="application/vnd.ms-excel")
-
